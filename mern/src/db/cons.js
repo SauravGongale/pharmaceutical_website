5838897759fb47945f10d84e155c81078734a036
imnakul99 <81411445+imnakul99@users.noreply.github.com>
2022-12-05 10:26:37 +0530

    Delete mern/src directory

--- mern/src/db/cons.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://Pharma:pharma@cluster0.a2f2ko5.mongodb.net/test",(err)=>{
-	if(!err){
-		console.log("---Database Connected-----");
-	}else{
-		console.log("Connection Error");
-	}
-})
